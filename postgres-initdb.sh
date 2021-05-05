556959f796567b811e212a3ca3fbc4199541488b
mihasik69 <Hht378h191god!>
2021-05-05 11:54:08 +0300

    docker compose v1.27

--- postgres-initdb.sh
+++
@@ -1,5 +0,0 @@
-psql --variable=ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    CREATE ROLE app42paas WITH LOGIN PASSWORD 'app42paas';
-    CREATE DATABASE "app42paas-api" OWNER = app42paas;
-    GRANT ALL PRIVILEGES ON DATABASE "app42paas-api" TO events;
-EOSQL
